696e3cfff6b567bcff1f180c0de1eac512ebab68
trung9902 <trung9a992002@gmail.com>
2023-09-17 22:12:09 +0700

    register

--- js/login.js
+++ js/login.js
@@ -1,38 +1,114 @@
 let loginForm = document.getElementById("login-form");
 let loginBtn = document.getElementById("login-btn");
 let errorMessage = document.getElementsByClassName("error-message");
+let LoginBox = document.querySelector(".login-box")
+let registedBox = document.querySelector(".Register")
+let backLogin = document.querySelector(".icon-back")
+// 
+// Login
+let ipUsername = document.querySelector(".username-input")
+let ipEmail = document.querySelector(".email-input")
+let ipPassword = document.querySelector(".password-input")
+// registed
+let ipUsernameRegisted = document.querySelector(".usernameRegisted-input")
+let ipEmailRegisted = document.querySelector(".emailRegisted-input")
+let ipPasswordRegisted = document.querySelector(".passwordRegisted-input")
+let addToArrayButton = document.getElementById("register-btn")
+// let Register = document.querySelector('.Register');
 
-console.dir(loginForm)
+let Login = () => {
+    let name = ipUsername.value;
+    let email = ipEmail.value;
+    let password = ipPassword.value;
+    // lấy data
+    const getLocalJson = localStorage.getItem('dataLogin');
+    const getLocal = JSON.parse(getLocalJson)
 
-loginBtn.addEventListener("click", (e) => {
-    e.preventDefault();
+    getLocal.forEach(item => {
+        if (name.length === 0 || email.length === 0 || password.length === 0) {
+            document.querySelector(".error-Green").innerHTML = ""
+        } else if (name !== item.name) {
+            document.querySelector(".error-Green").innerHTML = "Tên tài khoản không chính xác"
+        }
+        else if (email !== item.email) {
+            document.querySelector(".error-Green").innerHTML = "Email không chính xác"
+        }
+        else if (password !== item.password) {
+            document.querySelector(".error-Green").innerHTML = "Mật khẩu không chính xác"
+        }
+        else {
+            localStorage.username = name;
+            window.location.replace('index.html')
+        }
+    });
 
-    // reset error
-    for (let i = 0; i < errorMessage.length; i++) {
-        errorMessage[i].innerText = "";
+
+    // check validate
+    if (checkemptyError([ipUsername, ipEmail, ipPassword])) return check = false;
+    if (!checkEmailError(ipEmail)) return check = false;
+    else return check = true;
+
+}
+
+// data
+let data = []
+function saveLocalsorate(data) {
+    // chuyển thành chuỗi json
+    let jsonLogin = JSON.stringify(data);
+    // lưu vào localstorate
+    localStorage.setItem('dataLogin', jsonLogin);
+}
+function getLocalsorate() {
+    const getLocalJson = localStorage.getItem('dataLogin');
+    const getLocal = JSON.parse(getLocalJson)
+
+}
+
+
+
+// xử lý đăng ký và lưu thôngt tin 
+let handerRegisted = () => {
+    let name = ipUsernameRegisted.value;
+    let email = ipEmailRegisted.value;
+    let password = ipPasswordRegisted.value;
+    handerClearIp = () => {
+        name = ''
+        email = ''
+        password = ''
     }
-    // GET VALUE FROM INPUT
-    let username = loginForm.username.value;
-    let email = loginForm.email.value;
-    let password = loginForm.password.value;
-
-    if (username.length < 5) {
-        
-        errorMessage[0].innerText = "Tên tài khoản quá ngắn!";
-        return;
+    let validate = () => {
+        if (checkemptyError([ipUsernameRegisted, ipEmailRegisted, ipPasswordRegisted])) return check = false;
+        if (!checkEmailError(ipEmailRegisted)) return check = false;
+        else return check = true;
     }
-
-    if (email.length < 8) {
-        
-        errorMessage[1].innerText = "Email quá ngắn!";
-        return;
+    if (validate()) {
+        data.push({ name: name, email: email, password: password });
+        saveLocalsorate(data)
+        console.log(data);
+        ipUsernameRegisted.value = '';
+        ipEmailRegisted.value = '';
+        ipPasswordRegisted.value = '';
+        backToLogin()
     }
+}
 
-    if (password.length < 8) {
-       
-        errorMessage[2].innerText = "Mật khẩu quá ngắn!";
-        return;
-    }
-    localStorage.username = username;
-    window.location.replace('index.html')
-});
+///chuyển sang registed
+let openRegister = () => {
+    handerClearIp()
+    LoginBox.style.display = "none";
+    registedBox.style.display = "block";
+}
+//back về login
+let backToLogin = () => {
+    handerClearIp()
+    LoginBox.style.display = "block";
+    registedBox.style.display = "none";
+}
+let handerClearIp=()=>{
+    ipUsernameRegisted.value = '';
+    ipEmailRegisted.value = '';
+    ipPasswordRegisted.value = '';
+    ipUsername.value = '';
+    ipEmail.value = '';
+    ipPassword.value = '';
+}
